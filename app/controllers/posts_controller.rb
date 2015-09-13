c24d6b3e14fa196cc374e1779af3590eb33ea307
medev21 <mebenav88@gmail.com>
2015-09-13 08:52:28 -0500

    add edit update destroy in post controller, add edit template

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,22 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
   private
     def post_params
       params.require(:post).permit(:title, :iframe, :intro)
