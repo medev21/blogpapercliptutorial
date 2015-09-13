0e0286edba2e065b9e9d57385e02e5ae6da02ec8
medev21 <mebenav88@gmail.com>
2015-09-12 19:53:43 -0500

    add form and new templates in posts views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,7 @@ class PostsController < ApplicationController
 
   private
     def post_params
-      params.require(:post).permit(:title, :iframe, :description)
+      params.require(:post).permit(:title, :iframe, :intro)
     end
 
     def find_post
